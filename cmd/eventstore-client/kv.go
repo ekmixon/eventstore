f07a7d52ada41df02bb872c2d694d9cb7f3af87f
Pablo Mercado <odacremolbap@gmail.com>
2021-07-22 19:55:12 +0200

    setup kong cli

---
+++ cmd/eventstore-client/kv.go
@@ -0,0 +1,72 @@
+/*
+Copyright (c) 2021 TriggerMesh Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/triggermesh/eventstore/pkg/client"
+)
+
+type KVCmd struct {
+	Set KVSetCmd `cmd:"" help:"Set Key/Value"`
+	Get KVGetCmd `cmd:"" help:"Set Key/Value"`
+	// DetachKeys string `help:"Override the key sequence for detaching a container"`
+	// NoStdin    bool   `help:"Do not attach STDIN"`
+	// SigProxy   bool   `help:"Proxy all received signals to the process" default:"true"`
+
+	// Container string `arg required help:"Container ID to attach to."`
+}
+
+type KVSetCmd struct {
+	Key   string        `help:"Key where the value will be stored" required:""`
+	Value string        `help:"Value to be stored" required:""`
+	TTL   time.Duration `help:"Key's time to live (seconds)" default:"5s"`
+}
+
+// type KVSet struct {
+// 	key   = kingpin.Flag("key", "Key for storing value.").Required().String()
+// 	value = kingpin.Flag("value", "Value to be stored.").Default("").String()
+// 	ttl   = kingpin.Flag("ttl", "Stored value's time to live (seconds).").Default("5").Int32()
+// }
+
+type KVGetCmd struct {
+}
+
+func (kv *KVSetCmd) Run(g *Globals) error {
+	fmt.Printf("Scope: %s\n", g.Scope)
+	fmt.Printf("Bridge: %s\n", g.Bridge)
+	fmt.Printf("Attaching to: %v\n", g.Instance)
+
+	c := client.New(g.Server, g.Timeout)
+	ctx := context.Background()
+	if err := c.Connect(ctx); err != nil {
+		log.Fatalf("Failed to dial %s: %v", g.Server, err)
+	}
+
+	defer func() { _ = c.Disconnect() }()
+
+	return nil
+}
+
+func (kv *KVGetCmd) Run(g *Globals) error {
+	fmt.Printf("KVGet Scope: %s\n", g.Scope)
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/triggermesh/eventstore
 go 1.15
 
 require (
+	github.com/alecthomas/kong v0.2.17 // indirect
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20210208195552-ff826a37aa15 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -12,7 +13,7 @@ require (
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	google.golang.org/genproto v0.0.0-20210714021259-044028024a4f // indirect
 	google.golang.org/grpc v1.39.0
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0
 	google.golang.org/protobuf v1.27.1
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
